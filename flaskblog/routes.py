31cbef93dd39607f3b4df55a78b7fd9efc970971
Bruno S Rodrigues <34436334+brusangues@users.noreply.github.com>
2020-12-02 00:17:16 -0300

    small update

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -106,3 +106,4 @@ def account():
         form.email.data = current_user.email
     image_file = url_for('static', filename='profile_pics/'+current_user.image_file)
     return render_template("account.html", title='account', image_file=image_file, form=form)
+
